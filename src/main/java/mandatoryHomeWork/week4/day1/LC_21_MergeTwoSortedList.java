2358bddc93896722e8c9d9bbb5d6dacd601282e3
shankar2408 <shankarec24@gmail.com>
2023-07-07 19:00:09 +0530

    code update

--- src/main/java/mandatoryHomeWork/week4/day1/LC_21_MergeTwoSortedList.java
+++ src/main/java/mandatoryHomeWork/week4/day1/LC_21_MergeTwoSortedList.java
@@ -1,100 +1,96 @@
 package mandatoryHomeWork.week4.day1;
 
-import org.testng.annotations.Test;
-
-import java.util.ArrayList;
-
-
+import org.junit.Test;
+import java.util.*;
 
 public class LC_21_MergeTwoSortedList {
 
 
-	    public class ListNode {
-	        int val;
-	        ListNode next;
-	        ListNode() {}
-	        ListNode(int val) { this.val = val; }
-	        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-	    }
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
 
-	    ListNode head = null, tail =null;
+    ListNode head = null, tail =null;
 
-	    public ListNode addAll(int[] input){
-	        for(int value: input)
-	            if(head == null)
-	                head = tail = new ListNode(value);
-	            else
-	                tail = tail.next = new ListNode(value);
-	        return head;
-	    }
+    public ListNode addAll(int[] input){
+        for(int value: input)
+            if(head == null)
+                head = tail = new ListNode(value);
+            else
+                tail = tail.next = new ListNode(value);
+        return head;
+    }
 
-	    @Override
-	    public String toString(){
-	        ArrayList<Integer> list = new ArrayList<>();
+    @Override
+    public String toString(){
+        ArrayList<Integer> list = new ArrayList<>();
 
-	        ListNode temp = head;
-	        while(temp != null) {
-	            list.add(temp.val);
-	            temp = temp.next;
-	        }
+        ListNode temp = head;
+        while(temp != null) {
+            list.add(temp.val);
+            temp = temp.next;
+        }
 
-	        return list.toString();
-	    }
+        return list.toString();
+    }
 
-	    private ListNode mergeList(ListNode head1, ListNode head2){
-
-
-	        ListNode head , tail;
-
-	        if(head1.val < head2.val){
-	            tail = head = new ListNode(head1.val);
-	            head1 = head1.next;
-	        }else{
-	            tail = head = new ListNode(head2.val);
-	            head2 = head2.next;
-	        }
+    private ListNode mergeList(ListNode head1, ListNode head2){
 
 
-	        while(head1 != null & head2 !=null){
-	            if(head1.val < head2.val){
-	                tail = tail.next =  new ListNode(head1.val);
-	                head1 = head1.next;
-	            }else{
-	                tail = tail.next =  new ListNode(head2.val);
-	                head2 = head2.next;
-	            }
-	        }
-	        print(head);
-	        if(head1 != null)
-	            tail.next = head1;
-	        else
-	            tail.next = head2;
+        ListNode head , tail;
 
-	        return head;
-	    }
+        if(head1.val < head2.val){
+            tail = head = new ListNode(head1.val);
+            head1 = head1.next;
+        }else{
+            tail = head = new ListNode(head2.val);
+            head2 = head2.next;
+        }
 
 
+        while(head1 != null & head2 !=null){
+            if(head1.val < head2.val){
+                tail = tail.next =  new ListNode(head1.val);
+                head1 = head1.next;
+            }else{
+                tail = tail.next =  new ListNode(head2.val);
+                head2 = head2.next;
+            }
+        }
+        if(head1 != null)
+            tail.next = head1;
+        else
+            tail.next = head2;
+       
+        return head;
+    }
 
-	    @Test
-	    public void test(){
-	        ListNode head1 = new MergeList().addAll(new int[]{1,2,4,5,6,8}),
-	                head2 = new MergeList().addAll(new int[]{1,3,4});
-
-	       ListNode newHead = mergeList(head1, head2);
-	        System.out.println(this);
-	    }
 
 
-	    private void print(ListNode node){
-	        while(node != null) {
-	            System.out.print(node.val + " ");
-	            node = node.next;
-	        }
-	        System.out.println();
-	    }
+    @Test
+    public void test(){
+        ListNode head1 = new LC_21_MergeTwoSortedList().addAll(new int[]{1,2,4,5,6,8}),
+                head2 = new LC_21_MergeTwoSortedList().addAll(new int[]{1,3,4});
 
+       ListNode newHead = mergeList(head1, head2);
+       print(newHead);
+    }
+
+
+    private void print(ListNode node){
+        while(node != null) {
+            System.out.print(node.val + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
 
 
 
 
-	}
+
+}
